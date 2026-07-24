98177151be31cb68839447f3bc80a8237a922fe1
walk-studios <brian@walkstudios.com>
2026-07-24 13:16:29 -0500

    add gitignore and sitemap, remove mma page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
-import netlify from '@astrojs/netlify';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import netlify from "@astrojs/netlify";
 
 export default defineConfig({
-  site: 'https://walkstudios.com',
-  integrations: [
-    tailwind(),
-    sitemap(),
-  ],
-  output: 'static',
+  site: "https://walkstudios.com",
+  integrations: [tailwind()],
+  output: "static",
   adapter: netlify(),
 });
--- dist/_astro/hoisted.BoLejxQO.js
+++
@@ -1,1 +0,0 @@
-import"./hoisted.V_dTjN99.js";const e=new URLSearchParams(window.location.search);e.get("success")==="true"&&document.getElementById("form-success")?.classList.remove("hidden");
